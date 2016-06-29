80865679e6139fd631fcec7736cb5891626a6b71
gbdb <guillaume25@gmail.com>
2016-06-29 19:29:18 -0400

    Add files via upload

---
+++ biot/dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+$MyUsername = "arduino";
+$MyPassword = "Bi0T2";
+$MyHostname = "localhost";	
+
+$dbh = mysqli_connect($MyHostname , $MyUsername, $MyPassword, "biot");
+//$bdd = mysqli_connect('serveur', $MyUsername, $MyPassword, "biot");
+//$selected = mysql_select_db("biot",$dbh);
+?>
